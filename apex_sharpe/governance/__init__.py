92c676ec88b00c1c7a4e134b69c14eec1dc476c3
mattyhtown <163786927+mattyhtown@users.noreply.github.com>
2026-09-24 07:34:22 -0400

    Export capital allocation and thesis lineage governance

--- apex_sharpe/governance/__init__.py
+++ apex_sharpe/governance/__init__.py
@@ -1,23 +1,19 @@
 """Governance primitives for capital protection and research graduation."""
 
 from .edge_ledger import (
-    Book,
-    EdgeClass,
-    EdgeRecord,
-    GraduationEvidence,
-    GraduationStage,
-    HistorianEvent,
-    append_historian_event,
-    evaluate_graduation,
+    Book, EdgeClass, EdgeRecord, GraduationEvidence, GraduationStage,
+    HistorianEvent, append_historian_event, evaluate_graduation,
+)
+from .capital_allocator import (
+    AllocationDecision, AllocationPolicy, AllocationRequest, allocate,
+)
+from .thesis_lineage import (
+    Thesis, ThesisRevision, may_mint_new_thesis, revise,
 )
 
 __all__ = [
-    "Book",
-    "EdgeClass",
-    "EdgeRecord",
-    "GraduationEvidence",
-    "GraduationStage",
-    "HistorianEvent",
-    "append_historian_event",
-    "evaluate_graduation",
+    "Book", "EdgeClass", "EdgeRecord", "GraduationEvidence", "GraduationStage",
+    "HistorianEvent", "append_historian_event", "evaluate_graduation",
+    "AllocationDecision", "AllocationPolicy", "AllocationRequest", "allocate",
+    "Thesis", "ThesisRevision", "may_mint_new_thesis", "revise",
 ]
